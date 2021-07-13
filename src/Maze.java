7f15b80cff7b3da0bf568a5f99908978a0a5610c
Punn Kamolyabutr <pkamolya@ucsd.edu>
2021-07-13 19:47:05 +0700

    first commit

---
+++ src/Maze.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Stack;
+
+public class Maze {
+
+}
---
+++ src/MazeGraphics.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MazeGraphics extends JPanel {
+    
+    
+    public static void main(String s[]) {
+
+    }
+    
+    
+    public void drawMaze() {
+        
+    }
+    
+}
